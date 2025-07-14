2b6be22ae8a14bb19d479bdbfb644c17363aa4ac
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2025-07-14 19:53:23 +0900

    feat: 주문(Order) Entity 생성

--- backend/src/main/java/com/back/domain/order/order/entity/Order.java
+++ backend/src/main/java/com/back/domain/order/order/entity/Order.java
@@ -1,4 +1,48 @@
 package com.back.domain.order.order.entity;
 
+/*
+
+ */
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
+    // 주문 고유 id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    // 주문한 사용자 정보
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // 주문 생성일시
+    private LocalDateTime createdAt;
+
+    // 주문 총 금액
+    private int totalPrice;
+
+    // 주문에 포함된 메뉴 리스트
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderMenu> orderMenus = new ArrayList<>();
+
+    // 주문 메뉴 추가
+    public void addOrderMenu(OrderMenu orderMenu) {
+        orderMenus.add(orderMenu);
+        orderMenu.setOrder(this);
+    }
 }
--- backend/src/main/java/com/back/domain/order/order/entity/OrderMenu.java
+++ backend/src/main/java/com/back/domain/order/order/entity/OrderMenu.java
@@ -1,4 +1,28 @@
 package com.back.domain.order.order.entity;
 
+import jakarta.persistence.*;
+import lombok.Setter;
+
+import java.awt.*;
+
+@Entity
+@Setter
 public class OrderMenu {
+    // 주문 메뉴 고유 id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    // 이 주문 메뉴가 속한 주문
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    // 주문한 메뉴의 정보
+    @ManyToOne
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
+
+    // 주문한 메뉴의 수량
+    private int quantity;
 }
