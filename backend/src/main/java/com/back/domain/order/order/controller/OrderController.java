128bab861f7ea7970bad96c956f10e347cf33351
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2025-07-15 14:29:45 +0900

    feat: Order 관련 엔티티, 레포지터리, 서비스, 컨트롤러 생성 , 주문 등록 구현

---
+++ backend/src/main/java/com/back/domain/order/order/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.back.domain.order.order.controller;
+
+import com.back.domain.order.order.dto.OrderRequestDto;
+import com.back.domain.order.order.dto.OrderResponseDto;
+import com.back.domain.order.order.serivce.OrderSerivce;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping
+public class OrderController {
+
+    private final OrderSerivce orderSerivce;
+
+    // Post /orders : 주문 등록
+    public OrderResponseDto createOrder(
+            @RequestBody OrderRequestDto orderRequestDto,
+            @AuthenticationPrincipal User user) {
+        return orderSerivce.createOrder(orderRequestDto, user);
+    }
+
+
+}
--- backend/src/main/java/com/back/domain/order/order/dto/OrderResponseDto.java
+++ backend/src/main/java/com/back/domain/order/order/dto/OrderResponseDto.java
@@ -2,30 +2,15 @@ package com.back.domain.order.order.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
-@Setter
-@NoArgsConstructor
+@AllArgsConstructor
 public class OrderResponseDto {
     private int orderId;
     private int totalPrice;
     private LocalDateTime createdAt;
-    private List<OrderItemsResponse> orderItems;
-
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class OrderItemsResponse {
-        private Integer menuId;
-        private String name;
-        private Integer quantity;
-        private Integer price;
-
-    }
+    private List<OrderMenuResponseDto> orderMenus;
 }
