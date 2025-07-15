38870c93749c8916dd66103fd7935d10b3fe4a5a
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2025-07-15 15:12:19 +0900

    fix: 주문등록 "/orders" 매핑 추가

--- backend/src/main/java/com/back/domain/order/order/controller/OrderController.java
+++ backend/src/main/java/com/back/domain/order/order/controller/OrderController.java
@@ -6,6 +6,7 @@ import com.back.domain.order.order.serivce.OrderSerivce;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class OrderController {
     private final OrderSerivce orderSerivce;
 
     // Post /orders : 주문 등록
+    @PostMapping("/orders")
     public OrderResponseDto createOrder(
             @RequestBody OrderRequestDto orderRequestDto,
             @AuthenticationPrincipal User user) {
