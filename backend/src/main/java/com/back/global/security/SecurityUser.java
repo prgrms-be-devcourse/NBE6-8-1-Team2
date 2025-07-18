4045a537e84fea6eeea81d3950a7652533eb515b
SoonTaeYouL <smile971021@naver.com>
2025-07-18 14:30:20 +0900

    로그인 인증
    # Conflicts:
    #	backend/src/main/java/com/back/domain/member/member/controller/MemberController.java
    #	backend/src/main/java/com/back/domain/member/member/service/MemberService.java
    #	backend/src/test/java/com/back/controller/MemberControllerTest.java

---
+++ backend/src/main/java/com/back/global/security/SecurityUser.java
@@ -0,0 +1,23 @@
+package com.back.global.security;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class SecurityUser extends User {
+    @Getter
+    private String email;
+    @Getter
+    private String nickname;
+    @Getter
+    private String address;
+
+    public SecurityUser(String email, String nickname, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(email, password, authorities);
+        this.email = email;
+        this.nickname = nickname;
+        this.address = address;
+    }
+}
