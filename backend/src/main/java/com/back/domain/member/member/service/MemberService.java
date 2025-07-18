b615f2a757672ec9efa429cd3bc605d08a02cb13
SoonTaeYouL <smile971021@naver.com>
2025-07-18 09:00:50 +0900

    기본 로그인 생성

--- backend/src/main/java/com/back/domain/member/member/service/MemberService.java
+++ backend/src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -2,7 +2,9 @@ package com.back.domain.member.member.service;
 
 import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.repository.MemberRepository;
+import com.back.global.exception.ServiceException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +17,7 @@ import java.util.Optional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Member join(String email, String password, String nickname, String address) {
         Member member = new Member(email, password, nickname, address);
@@ -35,5 +38,15 @@ public class MemberService {
 
     public void deleteById(int id) {
         memberRepository.deleteById(id);
+    }
+
+    public void checkPassword(Member member, String password) {
+        if(!passwordEncoder.matches(password, member.getPassword())) {
+            throw new ServiceException("401-1", "비밀번호가 일치하지 않습니다.");
+        }
+    }
+
+    public String genAccessToken(Member member) {
+        return null;
     }
 }
--- backend/src/test/java/com/back/controller/MemberControllerTest.java
+++ backend/src/test/java/com/back/controller/MemberControllerTest.java
@@ -51,7 +51,7 @@ public class MemberControllerTest {
                 .andExpect(handler().handlerType(MemberController.class))
                 .andExpect(handler().methodName("join"))
 //                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.resultCode").value("201-1"))
+                .andExpect(jsonPath("$.resultCode").value("200-1"))
                 .andExpect(jsonPath("$.msg").value("%s님 환영합니다. 회원가입이 완료되었습니다.".formatted(member.getEmail())))
                 .andExpect(jsonPath("$.data").exists())
                 .andExpect(jsonPath("$.data.email").value(member.getEmail()))
@@ -59,4 +59,33 @@ public class MemberControllerTest {
                 .andExpect(jsonPath("$.data.nickname").value(member.getNickname()))
                 .andExpect(jsonPath("$.data.address").value(member.getAddress()));
     }
+
+    @Test
+    @DisplayName("로그인")
+    void t2() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "email" : "user1@example.com",
+                                            "password" : "1234"
+                                        }
+                                        """.stripIndent())
+                ).andDo(print());
+
+        Member member = memberService.findByEmail("user1@example.com").get();
+
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("login"))
+                .andExpect(jsonPath("$.resultCode").value("200-1"))
+                .andExpect(jsonPath("$.msg").value("%s님 환영합니다.".formatted(member.getEmail())))
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.item").exists())
+                .andExpect(jsonPath("$.data.item.email").value(member.getEmail()))
+                .andExpect(jsonPath("$.data.item.nickname").value(member.getNickname()))
+                .andExpect(jsonPath("$.data.item.address").value(member.getAddress()));
+                }
 }
